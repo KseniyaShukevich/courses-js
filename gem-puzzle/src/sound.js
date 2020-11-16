d9225fff70a149aa854764a5fb6d268276d081f0
Kseniya Shukevich <KseniyaShukevich@gmail.com>
2020-11-17 01:27:00 +0300

    feat: settings for sound

--- gem-puzzle/src/sound.js
+++ gem-puzzle/src/sound.js
@@ -1,5 +1,6 @@
 import sound from './assets/sound/button6.wav';
 
+const soundSetting = document.querySelector('.sound-setting');
 let soundElement = {};
 
 function createSound() {
@@ -12,14 +13,42 @@ function getSound() {
   soundElement.play();
 }
 
+function setSound() {
+  const elements = document.querySelectorAll('.element');
+  elements.forEach((item) => {
+    item.addEventListener('click', getSound);
+  });
+}
+
 export default function addEventsSound() {
-  const arraySaveGame = localStorage.getItem('arraySaveGame');
-  if (!arraySaveGame || arraySaveGame === 'null') {
+  createSound();
+  if (!localStorage.getItem('saveSoundGame')) {
     localStorage.setItem('saveSoundGame', '1');
-    createSound();
-    const elements = document.querySelectorAll('.element');
+    setSound();
+  } else {
+    const savedSoundSetting = +localStorage.getItem('saveSoundGame');
+    if (savedSoundSetting === 1) {
+      setSound();
+    }
+  }
+}
+
+function getValueSound() {
+  const value = soundSetting.getAttribute('checked');
+  const elements = document.querySelectorAll('.element');
+  if (value) {
+    soundSetting.removeAttribute('checked');
+    localStorage.setItem('saveSoundGame', '0');
+    elements.forEach((item) => {
+      item.removeEventListener('click', getSound);
+    });
+  } else {
+    soundSetting.setAttribute('checked', 'checked');
+    localStorage.setItem('saveSoundGame', '1');
     elements.forEach((item) => {
       item.addEventListener('click', getSound);
     });
   }
 }
+
+soundSetting.addEventListener('click', getValueSound);
